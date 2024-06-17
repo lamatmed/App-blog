3b0974581236cdf9fb0660d30165dfc5ff4e190b
lamat <lamat032025@gmail.com>
2024-06-17 22:37:50 +0100

    first commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Category(models.Model):
+    name=models.CharField(max_length=120)
+    def __str__(self):
+        return self.name
+# Create your models here.
+class Article(models.Model):
+    title=models.CharField(max_length=50)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    desc=models.TextField()
+    image=models.ImageField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
